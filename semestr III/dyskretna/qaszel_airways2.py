5dc4c5bc387ba8522ec6c8f9ac5db2fd5ce7f36e
bbronek <barbro214@gmail.com>
2022-01-13 21:36:10 +0100

    Add qaszel_airways2

---
+++ semestr III/dyskretna/qaszel_airways2.py
@@ -0,0 +1,27 @@
+# Bartosz Bronikowski
+# Qaszel Airways 2 13.01.2022
+
+def main():
+    n, m =  map(int, input().split())
+    keys = [i for i in range(n)]
+    v = {key: [] for key in keys}
+
+    for x in range(m):
+        c = list(map(int, input().split()))
+        if (c[0] == 1):
+            x = c[1]
+            y = c[2]
+            if (not y in v[x]):
+                v[x].append(y)
+                v[y].append(x)
+        elif (c[0] == 4):
+            x = c[1]
+            print(len(v[x]))
+        elif (c[0] == 5):
+            x = c[1]
+            for city in sorted(v[x]):
+              print(city, end=" ")
+            print('')
+            
+if __name__ == '__main__':
+    main()
